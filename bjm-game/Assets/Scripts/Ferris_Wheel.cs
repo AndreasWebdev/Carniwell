8420de088afb18dd68a576d8eb7fd60d3a812b03
Sashify <sascha.wippis@googlemail.com>
2018-10-13 01:51:23 +0200

    Setup der Grundmap
    Riesenrad und so

---
+++ bjm-game/Assets/Scripts/Ferris_Wheel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ferris_Wheel : MonoBehaviour {
+
+
+    public bool active = false;
+    public float speed = 10; // Drehgeschwindigkeit
+    public Transform RotatingObject; //Das Hauptrad
+    public Transform[] cabins;
+	void Start () {
+		
+	}
+
+    public void Update()
+    {
+        if (!active) return;
+        RotatingObject.Rotate(Vector3.right * (speed * Time.deltaTime));
+
+       foreach(Transform cabin in cabins)
+        {
+            cabin.Rotate(-Vector3.right * (speed * Time.deltaTime));
+
+
+        }
+    }
+
+    //DIESE METHODE ZUM STARTEN DES DREHVORGANGS NUTZEN
+    public void Activate()
+    {
+        active = true;   
+    }
+
+    //DIESE METHODE ZUM STOPPEN DES DREHVORGANGS NUTZEN
+    public void Deactivate()
+    {
+        active = false;
+    }
+
+
+  
+}

55c9a8beabcce3aeb439fff78ca879bc9290783e
Sashify <sascha.wippis@googlemail.com>
2018-10-13 18:13:57 +0200

    VisitorManager im ParkManager Prefab

--- bjm-game/Assets/Scripts/ParkManager.cs
+++ bjm-game/Assets/Scripts/ParkManager.cs
@@ -6,6 +6,7 @@ public class ParkManager : MonoBehaviour {
 
     public List<AttractionController> activeAttractions = new List<AttractionController>();
 
+
 	// Use this for initialization
 	void Start () {
 		
---
+++ bjm-game/Assets/Scripts/VisitorManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisitorManager : MonoBehaviour {
+    public int visitorsSatisfied;
+    public float happinessPercentage;
+    [Header("Stages")]
+    public int baseVisitorsNeeded = 20;
+    public float multiplierPerStage = 2;
+    public int currentVisitorsNeeded;
+	// Use this for initialization
+	void Start () {
+        currentVisitorsNeeded = baseVisitorsNeeded;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AddSatisfiedVisitors(int _amount)
+    {
+        visitorsSatisfied += _amount;
+        if(visitorsSatisfied >= currentVisitorsNeeded)
+        {
+            NextStage();
+            
+        }
+    }
+    public void NextStage()
+    {
+        currentVisitorsNeeded = Mathf.RoundToInt(currentVisitorsNeeded * multiplierPerStage);
+        FindObjectOfType<ParkManager>().CreateNewAttraction();
+    }
+
+
+}

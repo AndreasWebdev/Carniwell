b03d8f5604b175345f7bb30e6ef053b0468dc2ba
Nel <einfachnur-nel@gmx.de>
2018-10-13 11:52:51 +0200

    playermovement

---
+++ bjm-game/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public Transform playerTransfrom;
+    public float cameraSmoothing = 3f;
+
+    Vector3 camPosOffset;
+
+    void Start ()
+    {
+        camPosOffset = transform.position - playerTransfrom.position;
+    }
+	
+	void FixedUpdate ()
+    {
+        Vector3 targetCamPos = playerTransfrom.position + camPosOffset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, cameraSmoothing * Time.deltaTime);
+    }
+}

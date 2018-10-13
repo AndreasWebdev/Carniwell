49a67184a6e323bedf5a7efc8d7ab97d5583e8ba
Andreas Heimann <andreas@iklikla.de>
2018-10-13 16:56:49 +0200

    Intro

---
+++ bjm-game/Assets/Scripts/LogoAnimTransition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LogoAnimTransition : MonoBehaviour {
+   
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Transition());
+	}
+	
+	IEnumerator Transition()
+    {
+        yield return new WaitForSeconds(6);
+        SceneManager.LoadScene("main");
+    }
+}

de77ffae566b58e68b1aebcb5624c75ef6cd61ec
herlex <HerleX@users.noreply.github.com>
2018-10-13 19:27:37 +0200

    NPC now works with ParkManager

--- bjm-game/Assets/Scripts/ParkManager.cs
+++ bjm-game/Assets/Scripts/ParkManager.cs
@@ -43,6 +43,7 @@ public class ParkManager : MonoBehaviour {
                 if (allBuildingSpots[i].myAttraction == null)
                 {
                     allBuildingSpots[i].BuildAttraction(attrToBuild);
+                    AddAttraction(attrToBuild);
                     break;
                 }
             }

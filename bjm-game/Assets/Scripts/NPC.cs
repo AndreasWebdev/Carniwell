c8279830af26afd90d01cd474b5a6f3b7e40bcd5
herlex <HerleX@users.noreply.github.com>
2018-10-13 02:10:48 +0200

    NPC Script
    erste Version des NPC Scripts

---
+++ bjm-game/Assets/Scripts/NPC.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour {
+
+    public enum status {
+        QUEUE,
+        WALKING,
+        ATTRACTION
+    }
+
+    private int happiness;
+    private status currentStatus;
+
+    private int nextUpdate = 1;
+
+    // Use this for initialization
+    void Start() {
+        happiness = 100;
+        currentStatus = status.WALKING;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // If the next update is reached
+        if (Time.time >= nextUpdate) {
+            nextUpdate = Mathf.FloorToInt(Time.time) + 1;
+            UpdateEverySecond();
+        }
+    }
+
+    // Update is called once per second
+    void UpdateEverySecond() {
+        if (currentStatus.Equals(status.QUEUE)) {
+            if (happiness > 0) {
+                --happiness;
+            }
+        } else if (currentStatus.Equals(status.ATTRACTION)) {
+            if (happiness < 100) {
+                ++happiness;
+            }
+        }
+    }
+
+    status getStatus() {
+        return currentStatus;
+    }
+
+    void setStatus(status newStatus) {
+        currentStatus = newStatus;
+    }
+
+    int getHappiness() {
+        return happiness;
+    }
+}

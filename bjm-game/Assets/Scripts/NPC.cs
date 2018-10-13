e1b0c993779fe78526e5bc1991600c6cc95b6e36
herlex <HerleX@users.noreply.github.com>
2018-10-13 04:06:35 +0200

    first version of AttractionController
    erste Version des Controller für Fahrgeschäfte, inklusive NPC Handling

--- bjm-game/Assets/Scripts/NPC.cs
+++ bjm-game/Assets/Scripts/NPC.cs
@@ -36,22 +36,26 @@ public class NPC : MonoBehaviour {
             if (happiness > 0) {
                 --happiness;
             }
-        } else if (currentStatus.Equals(status.ATTRACTION)) {
-            if (happiness < 100) {
-                ++happiness;
-            }
+        //} else if (currentStatus.Equals(status.ATTRACTION)) {
+        //    if (happiness < 100) {
+        //        ++happiness;
+        //    }
         }
     }
 
-    status getStatus() {
+    public status GetStatus() {
         return currentStatus;
     }
 
-    void setStatus(status newStatus) {
+    public void SetStatus(status newStatus) {
         currentStatus = newStatus;
     }
 
-    int getHappiness() {
+    public int GetHappiness() {
         return happiness;
+    }
+
+    public void AddHappiness(int reward) {
+        happiness += reward;
     }
 }

d39dc7bc37356554c9fdbda7d922d8a59ccd64e4
Nel <einfachnur-nel@gmx.de>
2018-10-13 19:02:26 +0200

    Update bjm-game/Assets/Scripts/PlayerMovement.cs

--- bjm-game/Assets/Scripts/PlayerMovement.cs
+++ bjm-game/Assets/Scripts/PlayerMovement.cs
@@ -34,13 +34,16 @@ public class PlayerMovement : MonoBehaviour {
             if (Physics.Raycast(touchRay, out floorHit, touchRayLength, floorMask))
             {
                 Vector3 moveDir = floorHit.point - transform.position;
-                moveDir.y = 0.2f;
 
+                Quaternion rotation = Quaternion.LookRotation(moveDir);
+                rotation.x = rotation.z = 0;
+                playerRigidbody.MoveRotation(rotation);
+
+                moveDir.y = 0.2f;
                 moveDir = moveDir.normalized * speed * Time.deltaTime;
                 playerRigidbody.MovePosition(transform.position + moveDir);
 
-                Quaternion rotation = Quaternion.LookRotation(moveDir);
-                playerRigidbody.MoveRotation(rotation);
+
 
                 anim.SetBool("moving",true);
                 

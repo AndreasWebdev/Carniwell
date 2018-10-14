a712c85d2c5f7458c7f2dae63b74b5b78782d5f6
Andreas Heimann <andreas@iklikla.de>
2018-10-14 17:38:33 +0200

    mainmenu stuff

--- bjm-game/Assets/Scripts/Menu/MenuManager.cs
+++ bjm-game/Assets/Scripts/Menu/MenuManager.cs
@@ -9,17 +9,56 @@ using UnityEngine.Timeline;
 public class MenuManager : MonoBehaviour
 {
     public PlayableDirector timelineCredits;
+    public bool isInCredits = false;
 
     public GameObject loadingPanel;
+
+    public Canvas canvasMain;
+    public Canvas canvasCredits;
+
+    public Animator settingsPanelAnim;
 
     private int nextUpdate = 1;
 
     private void Start() {
         loadingPanel.SetActive(false);
+        canvasMain.gameObject.SetActive(true);
+        canvasCredits.gameObject.SetActive(false);
     }
 
-    void buttonStart() {
+    private void Update()
+    {
+        if(isInCredits && timelineCredits.state != PlayState.Playing)
+        {
+            creditsEnd();
+        }
+    }
+
+    public void buttonStart() {
         StartCoroutine(LoadLevel());
+    }
+
+    public void buttonSettings()
+    {
+        settingsPanelAnim.SetBool("isActive", true);
+    }
+
+    public void buttonSettingsSave()
+    {
+        settingsPanelAnim.SetBool("isActive", false);
+    }
+
+    public void buttonCredits()
+    {
+        isInCredits = true;
+        timelineCredits.Play();
+        canvasMain.gameObject.SetActive(false);
+        canvasCredits.gameObject.SetActive(true);
+    }
+
+    public void creditsEnd()
+    {
+        SceneManager.LoadSceneAsync(0);
     }
 
     IEnumerator LoadLevel() {
@@ -32,7 +71,6 @@ public class MenuManager : MonoBehaviour
         while (!async.isDone) {
             if (Time.time >= nextUpdate) {
                 nextUpdate = Mathf.FloorToInt(Time.time) + 1;
-                updateLoadingPanel(cnt);
 
                 if (cnt == 2) {
                     cnt = 0;
@@ -45,23 +83,5 @@ public class MenuManager : MonoBehaviour
         }
 
         loadingPanel.SetActive(false);
-    }
-
-    void updateLoadingPanel(int state) {
-        Text loadingText = loadingPanel.GetComponentInChildren<Text>();
-        switch (state) {
-            case 0:
-                loadingText.text = "Loading page .";
-                break;
-            case 1:
-                loadingText.text = "Loading page ..";
-                break;
-            case 2:
-                loadingText.text = "Loading page ...";
-                break;
-            default:
-                // Nothing to do
-                break;
-        }
     }
 }

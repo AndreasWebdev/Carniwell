ef7872b1959ca20899c084e61cd00d6774b565b0
Sashify <13474512+Sashify@users.noreply.github.com>
2018-10-16 19:04:54 +0200

    Prep for SoundSettings

--- bjm-game/Assets/Scripts/Constants/PlayerPrefsConstants.cs
+++ bjm-game/Assets/Scripts/Constants/PlayerPrefsConstants.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 
 public static class PlayerPrefsConstants
 {
-
+    //Score
     public static string HighscoreTimeKey = "HighscoreTime";
 
+    //Settings
+    public static string SFXVolumeKey = "SFX_Volume";
+    public static string MusicVolumeKey = "Music_Volume";
+    public static string AnnouncerVolumeKey = "Announcer_Volume";
 
+    #region Highscore
     public static void SetHighscoreTime(float _score)
     {
         PlayerPrefs.SetFloat(HighscoreTimeKey, _score);
@@ -30,4 +35,43 @@ public static class PlayerPrefsConstants
 
         return timeString;
     }
+    #endregion
+
+    #region
+
+    public static void SetSFXVolume(float _vol)
+    {
+
+        _vol = Mathf.Clamp(_vol, 0, 1);
+        PlayerPrefs.SetFloat(SFXVolumeKey, _vol);
+    }
+
+    public static void SetMusicVolume(float _vol)
+    {
+        _vol = Mathf.Clamp(_vol, 0, 1);
+        PlayerPrefs.SetFloat(MusicVolumeKey, _vol);
+    }
+
+    public static void SetAnnouncerVolume(float _vol)
+    {
+        _vol = Mathf.Clamp(_vol, 0, 1);
+        PlayerPrefs.SetFloat(AnnouncerVolumeKey, _vol);
+    }
+
+    public static float GetSFXVolume()
+    {
+        return PlayerPrefs.GetFloat(SFXVolumeKey);
+    }
+
+    public static float GetMusicVolume()
+    {
+        return PlayerPrefs.GetFloat(MusicVolumeKey);
+    }
+
+    public static float GetAnnouncerVolume()
+    {
+        return PlayerPrefs.GetFloat(AnnouncerVolumeKey);
+    }
+
+    #endregion
 }
---
+++ bjm-game/Assets/Scripts/SoundSource.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundSource : MonoBehaviour {
+
+    public AudioSource audioSource;
+    public enum SoundType { SFX,Music,Announcer};
+    public SoundType soundType;
+
+    void Start () {
+
+        float currentVol = audioSource.volume;
+        
+        if(soundType == SoundType.SFX)
+        {
+            float pref = PlayerPrefsConstants.GetSFXVolume();
+            audioSource.volume = currentVol / 100 * (pref * 100);
+        }else if(soundType == SoundType.Music)
+        {
+            float pref = PlayerPrefsConstants.GetMusicVolume();
+            audioSource.volume = currentVol / 100 * (pref * 100);
+        }
+        else if(soundType == SoundType.Announcer)
+        {
+            float pref = PlayerPrefsConstants.GetAnnouncerVolume();
+            audioSource.volume = currentVol / 100 * (pref * 100);
+        }
+
+    }
+
+}

a4208907722427fd79588e5d4f9d711adf5c5db9
herlex <HerleX@users.noreply.github.com>
2018-10-18 16:11:51 +0200

    hide temporary overlays when not visible

---
+++ bjm-game/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AnimationController : MonoBehaviour
+{
+    public bool eventReceived = false;
+
+    CanvasGroup canvasGroup;
+
+     void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void freeze()
+    {
+        if (!eventReceived)
+        {
+            Time.timeScale = 0;
+            eventReceived = true;
+        }
+    }
+
+    public void Hide()
+    {
+        canvasGroup.alpha = 0f;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void Show()
+    {
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+    }
+}
